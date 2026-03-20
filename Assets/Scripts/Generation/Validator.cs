b21351872257db1598f4e3c794c62492df135e93
VOID\winmi <slavka779@mail.ru>
2026-03-20 21:50:19 +0700

    Check Neighbors in validator

---
+++ Assets/Scripts/Generation/Validator.cs
@@ -0,0 +1,40 @@
+﻿namespace Generation
+{
+	using System.Collections.Generic;
+
+	public class Validator
+	{
+		public void Validate(Grid grid)
+		{
+			Dictionary<Creature, bool> allCreatures = new();
+
+			foreach (var node in grid.Nodes.Values)
+			{
+				if (node?.Creature != null)
+					allCreatures.Add(node.Creature, false);
+			}
+
+			LinkedList<Creature> chain = new LinkedList<Creature>();
+
+			foreach (var creature in allCreatures.Keys)
+			{
+				chain.Clear();
+
+				chain.AddFirst(creature);
+
+				if (creature.OriginNode.Neighbors.TryGetValue(creature.Direction, out var neighborNode))
+				{
+					if (neighborNode.Neighbors.TryGetValue(neighborNode.CreatureDirection, out var targetNode))
+					{
+						if (chain.Last.Value == targetNode.Creature || chain.Contains(targetNode.Creature))
+						{
+							neighborNode.Creature.InvertDirection();
+						}
+					}
+
+					chain.AddLast(neighborNode.Creature);
+				}
+			}
+		}
+	}
+}
